5ab5174961eaf76f0f8ec63f6f29ad46485ad61a
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2022-11-26 13:45:50 +0100

    fix: change let to consst

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,6 +1,6 @@
 //A future calculator
 const yearOfBirth = 1984;
-let yearFuture = 2027;
+const yearFuture = 2027;
 let age = yearFuture - yearOfBirth;
 console.log(`You will be ${age} years old in ${yearFuture}`)
 
