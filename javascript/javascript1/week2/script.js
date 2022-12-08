931452df2fd5fcd83b03b696bc680cd5a445080a
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2022-12-08 13:36:55 +0100

    add Event application

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -13,3 +13,28 @@ function gettFullname (firstname, surname, useFormalName, gender) {
 let fullname1 = gettFullname('Leonardo', 'DiCaprio', true, 'male')
 let fullname2 = gettFullname('Angellina', 'Jolie', true, 'female')
 let fullname = gettFullname('Joey', 'Ttibbiani', false, 'male')
+
+//Event application
+
+let weekday = new Date();
+function getEventWeekday(event) {
+  
+const dayTitle = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const day = weekday.getDay();
+
+if(typeof event === 'number') {
+    if (event > (7 - day)){
+        let futureEventIndex = (event + day) % 7;
+        return `The event will be on ${dayTitle[futureEventIndex]}`;
+    }else if (event < (7 - day)){
+        let futureEventIndexLover = day + event;
+        return `The event will be on ${dayTitle[futureEventIndexLover]}`;
+    }else {
+        console.log('incorrect')
+    }
+}
+}
+getEventWeekday(12)
+
+
