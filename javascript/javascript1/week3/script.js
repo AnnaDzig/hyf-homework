b123be01b07dacb4d69c44e235a8f5287781b704
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2022-12-12 07:01:16 +0100

    add js file & Item array removal

---
+++ javascript/javascript1/week3/script.js
@@ -0,0 +1,18 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  
+  // Write some code here
+  
+  // Code done
+  
+  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+  
