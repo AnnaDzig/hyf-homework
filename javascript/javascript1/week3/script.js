d4f33c7f787255700fe57e239abd424e60f5261d
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2022-12-16 11:53:35 +0100

    add Series duration of my life

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -38,3 +38,40 @@ const names = [
   
   const travelTime = whenWillBeThere(travelInformation);
   console.log(travelTime); // 8 hours and 38 minutes
+
+  //Series duration of my life
+  const seriesDurations = [
+    {
+      title: "Friends",
+      days: 10,
+      hours: 4,
+      minutes: 20,
+    },
+    {
+      title: "The Big Bang Theory",
+      days: 14,
+      hours: 10,
+      minutes: 38,
+    },
+    {
+      title: "Breaking bad",
+      days: 14,
+      hours: 10,
+      minutes: 38,
+    },
+  ];
+ 
+  
+  function logOutSeriesText() {
+    const age = 80;
+    const ageInHours = age * 365 * 24;
+    let amount = 0;
+
+    for (let i = 0; i < seriesDurations.length; i++) {
+      let hoursTime = seriesDurations[i].days * 24 + seriesDurations[i].hours;
+      let persentOfLife = hoursTime * 100 / ageInHours;
+      console.log(`${seriesDurations[i].title} took ${persentOfLife} % of my life`);
+      }
+    }
+  
+  logOutSeriesText(); // logs out the text found above
