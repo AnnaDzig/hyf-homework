18d1e159c3c11ed36961db63cc34115454e5f763
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2023-01-11 22:43:04 +0100

    1.2. Find and count the Danish letters

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -6,3 +6,43 @@ function shortestDanishWord() {
 }
 
 console.log(shortestDanishWord()) ;
+
+//1.2. Find and count the Danish letters
+
+const danishString = "Jeg har en blå bil";
+const letter = 'å'
+const danishString2 = "Blå grød med røde bær";
+
+const returnObject = {};
+
+
+
+function oneLetterCounter(){
+    let count = 1;
+    for (let i = 0; i < danishString.length; i++) {
+      if (danishString[i] === letter[i + 1]) {
+        count++;
+      } else {
+        console.log(`total:${danishString.length} 'å' ${count} times`);
+        count = 1;
+      }
+    }
+}
+oneLetterCounter();
+
+function letterCounter() {
+        // loop over input string
+        for (let i = 0; i < danishString2.length; i++) {
+          // check if character has been seen before
+          if (returnObject.hasOwnProperty(danishString2[i])) {
+            // increase its count by 1
+            returnObject[danishString2[i]] += 1;
+          } else {
+            // add it to the object with a count of 1
+            returnObject[danishString2[i]] = 1;
+          }
+        }
+        return returnObject;
+      }; 
+
+console.log(letterCounter());
